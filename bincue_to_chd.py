4757436bc880b8f4f22da694c87bcdd8a6c7fb81
Hunter King <hking0036@gmail.com>
2023-04-07 23:54:43 -0400

    refactor for pathlib, support windows

--- bincue_to_chd.py
+++ bincue_to_chd.py
@@ -4,32 +4,45 @@ import glob
 import re
 import subprocess
 import shutil
+import pathlib
 
 parser = argparse.ArgumentParser(
     prog = "Bin/Cue to CHD",
     description = "Batch converts bin/cues to CHD."
 )
 
-parser.add_argument('path')
+
 parser.add_argument('-r', '--recursive', action = 'store_true')
+parser.add_argument('-w', '--windows', action = 'store_true')
+parser.add_argument('path')
 args = parser.parse_args()
+
+WINDOWS_CHDMAN = ".\chdman.exe"
+LINUX_CHDMAN = "chdman"
+target_chdman = ""
+
+if(args.windows):
+    target_chdman = WINDOWS_CHDMAN
+else:
+    target_chdman = LINUX_CHDMAN
 
 def parse_directory(directory_base, files):
     bad_files = []
     for file in files:
+        print(f"Evaluating file {file}")
         if(".cue" in file):
             disc_tag = re.search("\(Disc.*\)", file)
             if(disc_tag is not None):
                 # CHD in place, remove .bin/.cues
                 print(directory_base)
                 filestem = file[0:file.find(".cue")]
-                infile = directory_base + "/" + file 
-                outfile = directory_base + "/" + filestem + ".chd"
-                subprocess.run(["chdman", "createcd", "-i", infile, "-o", outfile ])
-                target_bins = glob.glob(directory_base + "/" + filestem + "*.bin")
+                infile = directory_base / pathlib.Path(file) 
+                outfile = directory_base / pathlib.Path( filestem + ".chd" )
+                subprocess.run([target_chdman, "createcd", "-i", infile, "-o", outfile ])
+                target_bins = pathlib.Path.glob( directory_base, (filestem + "*.bin") ) 
                 for bin in target_bins:
                     os.remove(bin)
-                os.remove(directory_base + "/" + filestem + ".cue")
+                os.remove(directory_base / pathlib.Path(filestem + ".cue"))
 
             else:
                 # CHD, move up directory, remove directory with .bin/.cue
@@ -37,10 +50,11 @@ def parse_directory(directory_base, files):
                 outfile = file[0:file.find(".cue")]
                 print(outfile)
                 print(f"Will write to {directory_base}/../")
-                infile = directory_base + "/" + file 
-                outfile = directory_base + "/" + file[0:file.find(".cue")] + ".chd"
-                subprocess.run(["chdman", "createcd", "-i", infile, "-o", outfile ])
-                shutil.move( outfile, (directory_base + "/../"))
+                filestem = file[0:file.find(".cue")]
+                infile = directory_base / pathlib.Path(file) 
+                outfile = directory_base / pathlib.Path( filestem + ".chd" )
+                subprocess.run([target_chdman, "createcd", "-i", infile, "-o", outfile ])
+                shutil.move( outfile, (directory_base / pathlib.Path("../")) )
                 shutil.rmtree(directory_base)
 
 for root, dirs, files in os.walk(args.path):
@@ -49,11 +63,11 @@ for root, dirs, files in os.walk(args.path):
     if(files is not []):
         if(not args.recursive):
             print("Only operating on root directory")
-            parse_directory(root, files)
+            parse_directory(pathlib.Path(root), files)
             break
         else:
             # FIXME: We will recursively walk through all of the directories we make in this process as well.
             # It's not the end of the world, because we check for a 7z extension, but it would be good to fix this.
-            parse_directory(root, files)
+            parse_directory(pathlib.Path(root), files)
 
 print(f"Failed to extract {bad_files}")
--- redump.py
+++ redump.py
@@ -2,13 +2,17 @@ import argparse
 import os
 import py7zr
 import re
+import pathlib
+
+# TODO: Python will handle my garbage paths for NT/POSIX for now, but in the future this should really use Pathlib.
 
 parser = argparse.ArgumentParser(
     prog = "Redump/No-Intro Extraction Tool",
     description = "Unpacks .7z files into directories for disc based games",
 )
-parser.add_argument('path')
+
 parser.add_argument('-r', '--recursive', action = 'store_true')
+parser.add_argument('path')
 args = parser.parse_args()
 
 
@@ -21,28 +25,33 @@ def parse_directory(directory_base, files):
             disc_tag = re.search("\(Disc.*\)", file)
             rev_tag = re.search("\(Rev.*\)", file)
             if(disc_tag is not None):
+                print(f"Found multidisc: {file}")
                 # Create a directory from the stub of this and then find all other games with this name.
-                target_dir = directory_base + "/" + file[0:disc_tag.start() - 1] # disc_tag.start() - 1 will remove the trailing space.
+                target_dir = directory_base / pathlib.Path( file[0 : disc_tag.start() - 1] )# disc_tag.start() - 1 will remove the trailing space.
 
                 # If the directory doesn't exist, see if there is a directory with the revision name instead.
                 if not os.path.exists( target_dir ) and rev_tag is not None:
-                    target_dir = directory_base + "/" + file[0:file.disc_tag.start() - 1] + " " + rev_tag.group(0)
+                    target_dir = directory_base / ( pathlib.Path( file[0: disc_tag.start() - 1] + " " + rev_tag.group(0) ))
                     if(not os.path.exists( target_dir )):
                         os.mkdir(target_dir)
                 elif(not os.path.exists(target_dir)):
                     os.mkdir(target_dir)
             else:
+                print(f"found single disc: {file}")
                 # Extract in place.
-                target_dir = directory_base + "/" + file[0:(file.find(".7z"))]
+                target_dir = directory_base / pathlib.Path( file[0:(file.find(".7z"))] )
                 if not os.path.exists(target_dir):
                     os.mkdir(target_dir)
             
             # Check if this has already been handled previously
-            cue_file = target_dir + "/" + file[0:file.find(".7z")] + ".cue"
+            cue_name = file[0:file.find(".7z")] + ".cue"
+            print(f"File stem: {cue_name}")
+            cue_file = target_dir / pathlib.Path( cue_name )
+            print(f"Target cue file is: {cue_file}")
             if(not os.path.exists( cue_file )):
                 print(f"Extracting {file} to {target_dir}")
                 try:
-                    with py7zr.SevenZipFile(directory_base + "/" + file, 'r') as archive:
+                    with py7zr.SevenZipFile(directory_base / pathlib.Path(file), 'r') as archive:
                         archive.extractall(path = target_dir)
                 except:
                     print(f"Failed to extract {file}")
@@ -56,11 +65,11 @@ for root, dirs, files in os.walk(args.path):
     target_dir = ""
     if(not args.recursive):
         print("Only operating on root directory")
-        parse_directory(root, files)
+        parse_directory(pathlib.Path(root), files)
         break
     else:
         # FIXME: We will recursively walk through all of the directories we make in this process as well.
         # It's not the end of the world, because we check for a 7z extension, but it would be good to fix this.
         if(files is not []):
-            parse_directory(root, files)
+            parse_directory(pathlib.Path(root), files)
 print(f"Failed to extract {bad_files}")
